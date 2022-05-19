4081cf6e2d8a9bcec49ca6395e734ce957a19778
DevDodger <reidlmiller@gmail.com>
2022-05-19 14:03:22 -0700

    SecurityUpdate

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Support</title>
+</head>
+    <body>
+        <main>
+            <form>
+                In Development Contact @devdodger
+                email = infoObfuscatorPlus@gmail.com
+                personal = reidlmiller@gmail.com
+            </form>
+        </main>
+    </body>
+</html>
