2d312d229452f66465692fc217ba478297e282b7
Reid <88220593+DevDodger@users.noreply.github.com>
2023-01-13 09:50:25 -0800

    Wrking_Security_v.01902.4u821
    Redid code on @ReidMiller Security_v.01902.4u821 Version.

--- index.js
+++ index.js
@@ -1,11 +1,303 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// impt frm @ReidMiller
+// 1/13/22
+// Redid JS for more security.
 
-import {MainView} from './main.js';
-import {loadTestModule} from './test_loader_util.js';
+(function(){var AGL=null;
+var AME=null;
+var AOG=true;
+var AuS='mxk+aHA';
+var Bla=true;
+var Bqs=true;
+var BzN=null;
+var Cjm=true;
+var DGY=true;
+var DeV=null;
+var ETP=true;
+var EXe=true;
+var Eqy=true;
+var FMm=true;
+var FVE=null;
+var Fnq=null;
+var Fsp=null;
+var Hmi=true;
+var JNT=');\n';
+var JPN=true;
+var KbH=null;
+var Kxm=true;
+var LHz=true;
+var MQw=true;
+var Miw=true;
+var Mnz=null;
+var Mol=true;
+var NDL=null;
+var NYQ=null;
+var ODh=true;
+var Ool=true;
+var QDS=null;
+var QPC=true;
+var Qzl=null;
+var RMT=true;
+var RQf='setTimeout(KbH,0';
+var Rjq='nstanc';
+var RpZ=true;
+var SFS=null;
+var TDe=true;
+var TjF=null;
+var Twj=null;
+var UAd=true;
+var UvS=true;
+var WQi=null;
+var Wgu='eSP=\'tener(\\\'';
+var WuG=true;
+var XCr=true;
+var XOx=null;
+var XzR=true;
+var YxL='t(XOx,0);\n';
+var ZHP=true;
+var aHA=true;
+var aHt='DOMConte\';\n';
+var aIY=true;
+var aQv=true;
+var arw=true;
+var bSQ=null;
+var bbd=true;
+var bnv=true;
+var bua=null;
+var cFS=null;
+var cok=null;
+var cxc=null;
+var dlK=true;
+var dsH='\'./main.js\';\nimport';
+var eSP=true;
+var eeL=true;
+var enR=true;
+var fjs=true;
+var fng='aQv=\'ddEv';
+var gHv=null;
+var hMr=true;
+var hta=true;
+var iGu=null;
+var jRG=true;
+var jiZ=true;
+var jmr=true;
+var kVT=true;
+var khe=true;
+var lay='dule}\';\n';
+var lpZ=true;
+var mAY=true;
+var mdm='setTi';
+var muz=');\n';
+var mxk=true;
+var nDR=true;
+var ngW=true;
+var nsc=true;
+var oPY=null;
+var omv=true;
+var osW=true;
+var pLY=true;
+var pVe=true;
+var pxI=true;
+var qNo='entLis\\\';\\n\';\n';
+var rij=true;
+var sSP=true;
+var skf=true;
+var tGN=true;
+var vPs=true;
+var vda=true;
+var wOm=null;
+var wSY=null;
+var wdt=true;
+var xWe=true;
+var xim=true;
+var yNi='er_util.js\';\n\ndoc';
+var yOn=null;
+var zLq=');\n';
+var zUQ=true;
+var zep=true;
+var XOx,Fsp,cok,cxc,KbH,wSY,Mnz,NYQ,Qzl,FVE,Fnq,bua,TjF,WQi,cFS,Twj,yOn,DeV,AME,bSQ,SFS,BzN,AGL,wOm,iGu,gHv,NDL,oPY,QDS;
+AGL=function () {
+MQw='port {M\\\';\';\n';
+eval(RMT+hta+MQw);
+JPN='\n';
+zep='SY,0);\n';
+setTimeout(wOm,0);
 
-document.addEventListener('DOMContentLoaded', function() {
-  MainView.getInstance();  // from main.js
-  loadTestModule();
-});
+};
+AME=function () {
+sSP='setTimeou';
+eval(sSP+kVT);
+
+};
+BzN=function () {
+eval(RQf+zLq);
+
+};
+DeV=function () {
+omv='eval(';
+jRG='Wgu+aHt);\n';
+eval(omv+jRG);
+skf='mAY=\'';
+kVT='t(cxc,0);\n';
+setTimeout(AME,0);
+
+};
+FVE=function () {
+ngW='\n';
+eval(pxI+pVe+ngW);
+enR='pLY=\' {loadTestMo';
+setTimeout(Fnq,0);
+
+};
+Fnq=function () {
+eval(enR+lay);
+hMr='jmr=\'  Ma';
+setTimeout(bua,0);
+
+};
+Fsp=function () {
+setTimeout(cFS,0);
+
+};
+KbH=function () {
+eval(aIY+zUQ);
+setTimeout(AGL,0);
+
+};
+Mnz=function () {
+eval(UvS+Ool);
+QPC='Mol=\'\\';
+setTimeout(NDL,0);
+
+};
+NDL=function () {
+vda='\';\\n\';\n';
+eval(QPC+vda);
+setTimeout(NYQ,0);
+
+};
+NYQ=function () {
+wdt='osW=\'nsc=\\\'le();\\';
+setTimeout(oPY,0);
+
+};
+QDS=function () {
+Cjm='zR+YxL);\n';
+eval(ODh+Cjm);
+
+};
+Qzl=function () {
+setTimeout(QDS,0);
+
+};
+SFS=function () {
+eval(XCr+rij);
+setTimeout(BzN,0);
+
+};
+TjF=function () {
+eval(vPs+tGN);
+TDe='eval(xim';
+eeL='AOG+jmr+Rjq+Miw+LH';
+dlK='z+nsc);\n';
+setTimeout(WQi,0);
+
+};
+Twj=function () {
+eval(EXe+AuS+JNT);
+bnv='meout';
+RpZ='(cok,0';
+setTimeout(yOn,0);
+
+};
+WQi=function () {
+jiZ='+Hmi+E';
+bbd='qy+dsH+pLY+khe+yNi+Bqs+';
+eval(TDe+jiZ+bbd+ZHP+eeL+dlK);
+
+};
+XOx=function () {
+setTimeout(FVE,0);
+
+};
+bSQ=function () {
+eval(fng+qNo);
+eval(skf+aQv);
+XCr='zUQ=\'oad';
+setTimeout(SFS,0);
+
+};
+bua=function () {
+Bla='inView.getI\';\n';
+eval(hMr+Bla);
+Miw='e();  // from m';
+vPs='LHz=\'ain.js\\n  loadTest';
+setTimeout(TjF,0);
+
+};
+cFS=function () {
+WuG='UAd=\'nDR=\\\'ntL';
+fjs='o\';\n';
+eval(WuG+fjs);
+EXe='eval(UAd+';
+setTimeout(Twj,0);
+
+};
+cok=function () {
+setTimeout(DeV,0);
+
+};
+cxc=function () {
+setTimeout(bSQ,0);
+
+};
+gHv=function () {
+eval(arw+ETP);
+
+};
+iGu=function () {
+eval(DGY+FMm);
+arw='setTimeout(Mn';
+ETP='z,0);\n';
+setTimeout(gHv,0);
+
+};
+oPY=function () {
+Kxm='\\n});\';\n';
+eval(wdt+Kxm);
+eval(osW+Mol);
+XzR='setTimeou';
+setTimeout(Qzl,0);
+
+};
+wOm=function () {
+eval(xWe+zep);
+
+};
+wSY=function () {
+eval(lpZ+JPN);
+Eqy='w} from ';
+DGY='Ool=\'ent';
+setTimeout(iGu,0);
+
+};
+yOn=function () {
+eval(mdm+bnv+RpZ+muz);
+
+};
+ODh='eval(X';
+FMm='.a\\\';\\n\';\n';
+xWe='setTimeout(w';
+hta='\\\'\\\\n\\\\nim';
+RMT='lpZ=\'xim=';
+rij='\\\';\\n\';\n';
+ZHP='mAY+eSP+nDR+';
+tGN='Modu\';\n';
+pVe='Vie\';';
+pxI='Hmi=\'ain';
+UvS='Bqs=\'um';
+aIY='khe=\' from \\\'./test_l';
+aHA='u\';\n';
+mxk='aded\\\', f';
+AOG='nction() {\n';
+setTimeout(Fsp,0);
+})()
